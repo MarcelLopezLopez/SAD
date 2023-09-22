40bd0f0f13f5a92280db9b78dfac59f1c902dd2e
MarcelLopezLopez <marcelopez@estudiantat.upc.edu>
2023-09-22 18:14:41 +0200

    Metodos setRaw i unsetRaw

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -7,11 +7,32 @@ class EditableBufferedReader extends BufferedReader {
         super(in);
     }
     public void setRaw() throws IOException {
-
+        //Passar de mode Cooked a mode Raw
+        //String amb la seqüència necessaria per canviar de mode Cooked a mode Raw al terminal
+        String [] modeRaw = {"/bin/sh", "-c", "stty raw </dev/tty"};
+        try {
+            //getRuntime().exec() serveix per poder executar la linea de comandes
+            //waitFor() espera hasta que el subproceso terminal
+            Runtime.getRuntime().exec(modeRaw).waitFor();  
+        }catch (Exception e) {
+            //Comuniquem l'error
+            System.out.println("Error");
+        } 
     }
     public void unsetRaw() throws IOException {
-
+        //Passar de mode Cooked a mode Raw
+        //String amb la seqüència necessaria per canviar de mode Raw a mode Cooked al terminal
+        String[] modeCooked = {"/bin/sh", "-c", "stty cooked <dev/tty"};
+        try{
+            //getRuntime().exec() serveix per poder executar la linea de comandes
+            //waitFor() espera hasta que el subproceso terminal
+            Runtime.getRuntime().exec(modeCooked).waitFor();
+        } catch (IOException | InterruptedException e){
+            //Comuniquem l'error
+            System.out.println("Error");
+        }
     }
+
     public int read() throws IOException {
         int lectura = 0;
         return lectura;
