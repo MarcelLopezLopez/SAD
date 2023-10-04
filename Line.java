c8e4148eb7606ee87cfb7a17fcf1f1dc1adf3284
MarcelLopezLopez <marcelopez@estudiantat.upc.edu>
2023-10-04 19:02:06 +0200

    Start Line

--- Line.java
+++ Line.java
@@ -1,13 +1,15 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Line {
     
     private int pos;
     private boolean insert;
-    ArrayList<> linia = new ArrayList<Char>();
-    Line(){
-        
+    private ArrayList<Character> linia;
 
+    Line(){
+        this.pos = 0;
+        this.insert = true;
+        this.linia = new ArrayList<Character>();
     }
 }
